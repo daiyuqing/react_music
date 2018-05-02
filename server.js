03a7bcbc3440e24ecc9516aef9b8af2d60f5f1a5
daiyuqing <765804005@qq.com>
2018-05-02 23:54:36 +0800

    init

---
+++ server.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var webpackDevMiddleware = require("webpack-dev-middleware");
+var webpackDevServer = require('webpack-dev-server');
+var config = require("./webpack.config.js");
+
+// 在入口文件数组中添加两个选项
+// webpack-dev-server/client?http://localhost:3000
+// webpack/hot/dev-server
+config.entry.index.unshift('webpack-dev-server/client?http://localhost:3000', 'webpack/hot/dev-server');
+
+  // 合并一个 devServer到配置文件
+Object.assign(config, {
+  devServer: {
+    hot: true,
+    inline: true
+  }
+})
+
+// 编译
+var compiler = webpack(config);
+
+// 初始化一个webpack-dev-server
+new webpackDevServer(compiler, {
+    publicPath: config.output.publicPath,
+    historyApiFallback: false,
+    stats: {
+      colors: true
+    }
+  }).listen(3000, 'localhost', function (error) {
+  if (error) {
+    console.error(error);
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,11 @@ const HtmlWebpackPlugin=require('html-webpack-plugin');
 const webpack=require('webpack');
 const path=require('path');
 const OpenBrowserPlugin=require('open-browser-webpack-plugin');
-
+const ROOT_PATH = path.resolve(__dirname);
+const SRC_PATH = path.resolve(ROOT_PATH, 'src');
 const config={
 	mode: 'development',
-	entry:{
-		app:['webpack-dev-server/client?http://localhost:3000/','./src/index.js']
-	},
+	entry:{index:['./src/index.js']},
 	output:{
 		filename:'bundle.js',
 		path:path.join(__dirname, 'build')
@@ -17,16 +16,15 @@ const config={
 	module:{
 		rules:[
 			{ 
-				test: /\.(js|jsx)$/, 
-				use: [
-					{
-						loader:'babel-loader' ,
-						options:{ 
-							presets: ['es2015', 'stage-0','react']
-						}
-					}
-				],
-				exclude: /node_modules/
+				test: /\.js$/,
+	            exclude: path.resolve(SRC_PATH, 'node_modules'),
+	            include: SRC_PATH,
+	            use: {
+	                loader: 'babel-loader',
+	                options: {
+	                    presets: ['react', 'es2015', 'stage-0']
+	                }
+	            }
 			},
 		]
 	},
@@ -38,18 +36,9 @@ const config={
     		template: './templates/index.html',
     		inject:'body'
     	}),
-    	new OpenBrowserPlugin({url:'http://localhost:3000'})
-	],
-	devServer: {
-        contentBase: path.join(__dirname, "../build"), //网站的根目录为 根目录/dist
-        port: 3000, //端口改为3000
-        host: '192.168.0.103', //如果指定的host，这样同局域网的电脑或手机可以访问该网站,host的值在dos下使用ipconfig获取 
-        open:true, // 自动打开浏览器
-        index:'index.html', // 与HtmlWebpackPlugin中配置filename一样
-        inline:true, // 默认为true, 意思是，在打包时会注入一段代码到最后的js文件中，用来监视页面的改动而自动刷新页面,当为false时，网页自动刷新的模式是iframe，也就是将模板页放在一个frame中
-        hot:false,
-        compress:true //压缩
-    }
+    	new webpack.HotModuleReplacementPlugin(),
+    	new OpenBrowserPlugin({url: 'http://localhost:3000'})
+	]
 }
 
 
